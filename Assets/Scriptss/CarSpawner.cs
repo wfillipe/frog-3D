a56ed0b429e04ba5232934229c02b92ecce41c2a
wfillipe <wilson.fillipe@academico.ifpb.edu.br>
2025-11-01 19:08:40 -0300

    antes do novo script de destruir

---
+++ Assets/Scriptss/CarSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    [Header("Configurações")]
+    public GameObject carPrefab;       // prefab do carro
+    public Transform leftSpawnPoint;   // ponto de spawn da esquerda
+    public Transform rightSpawnPoint;  // ponto de spawn da direita
+    public float spawnInterval = 2f;   // tempo entre spawns
+    public float carSpeed = 5f;        // velocidade dos carros
+
+    private float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            SpawnCars();
+            timer = 0f;
+        }
+    }
+
+    void SpawnCars()
+    {
+        // spawn da esquerda > direita
+        GameObject carL = Instantiate(carPrefab, leftSpawnPoint.position, Quaternion.identity, transform);
+        carL.AddComponent<CarMovement>().Initialize(Vector3.right, carSpeed);
+
+        // spawn da direita > esquerda
+        GameObject carR = Instantiate(carPrefab, rightSpawnPoint.position, Quaternion.identity, transform);
+        carR.AddComponent<CarMovement>().Initialize(Vector3.left, carSpeed);
+    }
+}
